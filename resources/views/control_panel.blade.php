50b778063f8ffd916fa6af2df55f820b7c342204
galuhestya <galuhestya@gmail.com>
2015-09-24 14:22:32 +0700

    new Control Panel view

--- resources/views/control_panel.blade.php
+++ resources/views/control_panel.blade.php
@@ -12,24 +12,20 @@
         <div class="row">
             <div class="col-lg-12">
                 <h1 class="page-header">Control Panel
-                    <small>What you've done</small>
+                    <small>Uploaded PKM</small>
                 </h1>
               </div>
         </div>
 
         <!-- Team Members Row -->
         <div class="row">
-            <div class="col-lg-12">
-                <h2 class="page-header">Your PKM</h2>
-            </div>
-
             @foreach($pkm_uploaded_by_user as $pkm)
             <div class="col-lg-4 col-sm-6 text-center margin-auto">
-                <img class="img-circle img-responsive img-center" src="http://placehold.it/200x200" alt="">
                 <h3>{{$pkm->category}}
                     <small>{{$pkm->id}}</small>
                 </h3>
                 <p>{{$pkm->title}}</p>
+                <img class="img-responsive img-center" src="{{asset('img/'.$pkm->category)}}.png" alt="icon" width="100" height="100">
                 <a class="btn btn-primary" href="/pkm/view/{{$pkm->id}}">View <span class="glyphicon glyphicon-chevron-right"></span></a>
                 <a class="btn btn-warning" href="/pkm/edit/{{$pkm->id}}">Edit <span class="glyphicon glyphicon-chevron-right"></span></a>
             </div>
